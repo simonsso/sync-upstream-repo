02aad622b5040033a46b71e4d305f0e51a818381
Fredrik Simonsson <simson@thesimson.net>
2024-04-01 16:35:36 +0900

    Refactor to use git reset --hard

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ UPSTREAM_BRANCH=$2
 DOWNSTREAM_BRANCH=$3
 GITHUB_TOKEN=$4
 FETCH_ARGS=$5
-MERGE_ARGS=$6
-PUSH_ARGS=$7
 SPAWN_LOGS=$8
 
 if [[ -z "$UPSTREAM_REPO" ]]; then
@@ -43,28 +41,8 @@ git remote -v
 
 git checkout ${DOWNSTREAM_BRANCH}
 
-case ${SPAWN_LOGS} in
-  (true)    echo -n "sync-upstream-repo https://github.com/dabreadman/sync-upstream-repo keeping CI alive."\
-            "UNIX Time: " >> sync-upstream-repo
-            date +"%s" >> sync-upstream-repo
-            git add sync-upstream-repo
-            git commit sync-upstream-repo -m "Syncing upstream";;
-  (false)   echo "Not spawning time logs"
-esac
-
+git reset --hard upstream/${UPSTREAM_BRANCH}
 git push origin
-
-MERGE_RESULT=$(git merge ${MERGE_ARGS} upstream/${UPSTREAM_BRANCH})
-
-
-if [[ $MERGE_RESULT == "" ]] 
-then
-  exit 1
-elif [[ $MERGE_RESULT != *"Already up to date."* ]]
-then
-  git commit -m "Merged upstream"
-  git push ${PUSH_ARGS} origin ${DOWNSTREAM_BRANCH} || exit $?
-fi
 
 cd ..
 rm -rf work
